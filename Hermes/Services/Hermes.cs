c510797f152cbfeab5f06523bc5f3f726642681d
MartinRenz <martinrenz@outlook.com>
2025-12-04 20:43:19 -0300

    feature: Creates Hermes implementation, add serviceProvider and constructor

---
+++ Hermes/Services/Hermes.cs
@@ -0,0 +1,14 @@
+﻿using Hermes.Interfaces;
+
+namespace Hermes.Services
+{
+    public class Hermes : IHermes
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Hermes(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}

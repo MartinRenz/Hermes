08ab584befac3cf31dd4615283c5b35aa9dd9677
MartinRenz <martinrenz@outlook.com>
2025-12-04 20:48:57 -0300

    feature: Add DependencyInjection package and create Send implementation for Hermes

--- Hermes/Services/Hermes.cs
+++ Hermes/Services/Hermes.cs
@@ -1,4 +1,5 @@
 ﻿using Hermes.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Hermes.Services
 {
@@ -9,6 +10,30 @@ namespace Hermes.Services
         public Hermes(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
+        }
+
+        public async Task<TResponse> Send<TResponse>(
+            IRequest<TResponse> message,
+            CancellationToken cancellationToken = default)
+        {
+            var handlerType = typeof(IRequestHandler<,>)
+                .MakeGenericType(message.GetType(), typeof(TResponse));
+
+            dynamic handler = _serviceProvider.GetRequiredService(handlerType);
+
+            return await handler.Handle((dynamic)message, cancellationToken);
+        }
+
+        public async Task Send(
+            IRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var handlerType = typeof(IRequestHandler<>)
+                .MakeGenericType(request.GetType());
+
+            dynamic handler = _serviceProvider.GetRequiredService(handlerType);
+
+            await handler.Handle((dynamic)request, cancellationToken);
         }
     }
 }
